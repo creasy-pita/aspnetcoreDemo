53580fcc4d5fea6bb75299bc9bdcb6fda83ce81e
creasy-pita <hz_ljq@live.com>
2018-11-08 19:31:52 +0800

    Configuration实例练习1

---
+++ ef/SqliteWithExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Data.Common;
+using System.Data;
+
+namespace SqliteWithExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SQLiteConnection db = new SQLiteConnection("Data Source=E:\\work\\myproject\\netcore\\aspnetcoreDemo\\ef\\SqliteWithExample\\Dict2Anki.db;FailIfMissing=True;");
+            try
+            {
+                //string commandText = "select id,terms,time  deck dictionary from history";
+                string commandText = "select id,terms,time from history";
+                db.Open();
+
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(commandText, db);
+
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                foreach (DataRow row in dt.Rows)
+                {
+                    //Console.WriteLine($"id:{row["id"]}");
+                    //Console.WriteLine($"terms:{row["terms"]}");
+                    //Console.WriteLine($"time:{row["time"]}");
+                    //Console.WriteLine("------------------------------------");
+
+                    System.Diagnostics.Debug.WriteLine($"id:{row["id"]}");
+                    System.Diagnostics.Debug.WriteLine($"terms:{row["terms"]}");
+                    System.Diagnostics.Debug.WriteLine($"time:{row["time"]}");
+                    System.Diagnostics.Debug.WriteLine("------------------------------------");
+                }
+
+                Console.WriteLine(ds.Tables[0].Rows.Count);
+                Console.ReadKey();
+            }
+            finally
+            {
+                db.Close();
+            }
+            
+        }
+    }
+}
---
+++ fundamentals/configuration/Example1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigurationExample
+{
+    public class Program
+    {
+        public static Dictionary<string, string> arrayDict = new Dictionary<string, string>
+        {
+            {"array:entries:0", "value0"},
+            {"array:entries:1", "value1"},
+            {"array:entries:2", "value2"},
+            {"array:entries:4", "value4"},
+            {"array:entries:5", "value5"}
+        };
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddXmlFile("tvshow.xml");
+                    config.AddInMemoryCollection(arrayDict);
+                }
+                )
+                .UseUrls("http://localhost:59056")
+                .UseStartup<Startup>();
+    }
+}
