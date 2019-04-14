436800e372c35e1ddcdc3ba1062c6b7ef829618e
creasy-pita <hz_ljq@live.com>
2019-04-14 14:28:15 +0800

    加入mysql efcore code first的迁移方式  使用finbook的 AppUserDbContext 数据表结构配置

---
+++ ef/MysqlEFMigrate/Data/AppUserDbContext.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User.API.Model;
+
+
+namespace User.API.Data
+{
+    public class AppUserDbContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserTag> UserTags { get; set; }
+
+       // private ILogger<AppUserDbContext> _logger;
+
+        public AppUserDbContext(DbContextOptions<AppUserDbContext> options) : base(options)
+        {
+            //_logger = logger;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>(entity =>
+            {
+                entity.ToTable("Users").HasKey(e => e.Id);
+            });
+
+            modelBuilder.Entity<UserProperty>().Property(u => u.Key).HasMaxLength(100);
+            modelBuilder.Entity<UserProperty>().Property(u => u.Value).HasMaxLength(100);
+            modelBuilder.Entity<UserProperty>()
+                .ToTable("UserProperties")
+                .HasKey(u => new { u.Key, u.AppUserId, u.Value });
+
+            modelBuilder.Entity<UserTag>().Property(u => u.Tag).HasMaxLength(100);
+            modelBuilder.Entity<UserTag>()
+                .ToTable("UserTags")
+                .HasKey(u => new { u.UserId, u.Tag});
+
+            modelBuilder.Entity<BPFile>()
+                .ToTable("UserBPFiles")
+                .HasKey(u => new { u.Id });
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+
+
+    }
+
+
+}
---
+++ ef/MysqlEFMigrate/Model/UserTag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.API.Model
+{
+    public class UserTag
+    {
+        /// <summary>
+        /// 用户id
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 标签
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// 标签
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+    }
+}
